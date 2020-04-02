d39d1c4ef7950af8ed7e94c3fd2adbc44b6cb3a4
romluc <romulofilhofilho@gmail.com>
2020-04-02 19:34:37 -0300

    Start to retrieve data from api

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Header from './Header';
 import Home from './Home';
 import Genres from './Genres';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 function App() {
+  const [data, setData] = useState({});
+
+  useEffect(() => {
+    axios.get('/api').then(res => {
+      setData(res.data);
+    });
+  }, []);
+
   return (
     <Router>
       <div>
         <Header />
         <Route path='/' exact component={Home} />
         <Route path='/genres' component={Genres} />
+        <pre>{JSON.stringify(data)}</pre>
       </div>
     </Router>
   );
--- src/Genres.js
+++ src/Genres.js
@@ -1,9 +1,35 @@
-import React from 'react';
-
-// import { Container } from './styles';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Table } from 'reactstrap';
 
 const Genres = () => {
-  return <h1>Genres</h1>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('/api/genres').then(res => {
+      setData(res.data.data);
+    });
+  });
+  return (
+    <div>
+      <h1>Genres</h1>
+      <Table dark>
+        <thead>
+          <tr>
+            <th scope='col'>Id</th>
+            <th scope='col'>Nome</th>
+            <th scope='col'>Ações</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope='row'>1</th>
+            <td>Mark</td>
+          </tr>
+        </tbody>
+      </Table>
+      <pre>{JSON.stringify(data)}</pre>
+    </div>
+  );
 };
 
 export default Genres;
