65372d505127f6ce53347ddbb7fb11ebe9889dde
romluc <romulofilhofilho@gmail.com>
2020-04-02 10:00:02 -0300

    Initial design with reactstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Navbar, NavbarBrand } from 'reactstrap';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>My Series</h1>
+    <div>
+      <Navbar>
+        <NavbarBrand>My Series</NavbarBrand>
+      </Navbar>
     </div>
   );
 }
